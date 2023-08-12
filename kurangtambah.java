d4f659efbc8a145f7fc67665b22e03752c51b227
geraldrmg <141795880+geraldrmg@users.noreply.github.com>
2023-08-12 20:21:00 +0700

    Delete kurangtambah.java

--- kurangtambah.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class kurangtambah{
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String tambah = scn.nextLine();
-    }
-}
