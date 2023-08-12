0cd83d0a0b842c3b09a325fbf783ac319d4f17dc
geraldrmg <141795880+geraldrmg@users.noreply.github.com>
2023-08-12 20:12:02 +0700

    kurang tambah

---
+++ kurangtambah.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class kurangtambah{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String tambah = scn.nextLine();
+    }
+}
